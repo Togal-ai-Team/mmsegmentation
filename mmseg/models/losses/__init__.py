a30f916db27046cdbf5d2cee0e0705bd7aa551f8
ol <oleg@magentiq.com>
2023-04-25 11:48:43 +0200

    Fix typo

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -3,7 +3,7 @@ from .accuracy import Accuracy, accuracy
 from .boundary_loss import BoundaryLoss
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
-from multilabel_cross_entropy_loss import MultiLabelBCEWithLogitsLoss
+from .multilabel_cross_entropy_loss import MultiLabelBCEWithLogitsLoss
 from .dice_loss import DiceLoss
 from .focal_loss import FocalLoss
 from .lovasz_loss import LovaszLoss
