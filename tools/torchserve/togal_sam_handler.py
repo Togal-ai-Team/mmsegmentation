fa2f89fc18627d0b0e16d44f823931501776d8c7
ol <oleg@magentiq.com>
2023-05-11 17:57:17 +0200

    Add filtering and grid prompting for the model

--- tools/torchserve/togal_sam_handler.py
+++ tools/torchserve/togal_sam_handler.py
@@ -1,12 +1,161 @@
-import base64
-import mmcv
 import torch
 import logging
+import numpy as np
+import mmcv
+import requests
+import cv2
 
 from ts.torch_handler.base_handler import BaseHandler
 from sam.sam_inferencer import SamAutomaticMaskGenerator
 
 logging.basicConfig(filename='/home/model-server/my_log.log', level=logging.INFO)
+
+
+def cut_image(image):
+    height, width = image.shape[:2]
+    new_height = int(height * 0.875)
+    new_width = int(width * 0.875)
+    image = image[:new_height, :new_width]
+    return image
+
+
+def is_box_close_to_border(tile_border, box, threshold=0):
+    x, w, y, h = tile_border
+    bx, by, bw, bh = box
+
+    # distances between box sides and tile border
+    left_dist = bx - x
+    right_dist = x + w - (bx + bw)
+    top_dist = by - y
+    bottom_dist = y + h - (by + bh)
+
+    # if is smaller than the threshold
+    return any(d <= threshold for d in [left_dist, right_dist, top_dist, bottom_dist])
+
+
+def aspect_ratio(box):
+    w = box[2]
+    h = box[3]
+    if w > h:
+        return w / h
+    else:
+        return h / w
+
+
+def sliding_window_inference(img, mask_generator, step=256, window_size=512,
+                             area_size_filter=[200, 3000], aspect=4):
+    merged_boxes = []
+    stability_score = []
+    # Generate masks for each window
+    for x in range(0, img.shape[1], step):
+        for y in range(0, img.shape[0], step):
+            logging.info('+1iter')
+            crop = img[y:y + window_size, x:x + window_size]
+            prompt_grid = adjust_grid(crop)
+
+            logging.info('prompt_grid_sl_win: {}'.format(prompt_grid[0][0:5]))
+
+            mask_generator.point_grids = prompt_grid
+
+            masks = mask_generator.generate(crop)
+            border = [y, y + window_size, x, x + window_size]
+            for mask in masks:
+                if area_size_filter[0] < mask['area'] < area_size_filter[1]:
+                    # Shift the bbox to account for the crop
+                    bbox = mask['bbox'] + np.array([x, y, 0, 0])
+                    # if not is_box_close_to_border(border, bbox, threshold=1) and \
+                    # aspect_ratio(bbox) < aspect:
+                    if aspect_ratio(bbox) < aspect:
+                        merged_boxes.extend([[int(i) for i in bbox.tolist()]])
+                        # stability_score = mask['stability_score']
+
+    return merged_boxes
+
+
+# function to remove text from image
+def remove_text(image, ocr_boxes):
+    for box in ocr_boxes:
+        image[int(box[1]):int(box[3]), int(box[0]):int(box[2])] = 255
+    return image
+
+
+def skip_white_boxes(boxes, image, white_portion_th=0.95):
+    """bbox in xywh format"""
+    boxes_to_leave = []
+    # convert image to gray
+    image = mmcv.bgr2gray(image)
+    for box in boxes:
+        x, y, w, h = box
+        box_image = image[int(y):int(y + h), int(x):int(x + w)]
+
+        # get portion of white pixels
+        white_portion = np.sum(box_image == 255) / (w * h)
+
+        # if portion of white pixels is bigger then 95% return True
+        if white_portion > white_portion_th:
+            continue
+        else:
+            boxes_to_leave.append(box)
+
+    return boxes_to_leave
+
+
+def build_point_grid(n_per_side: int) -> np.ndarray:
+    """Generates a 2D grid of points evenly spaced in [0,1]x[0,1]."""
+    offset = 1 / (2 * n_per_side)
+    points_one_side = np.linspace(offset, 1 - offset, n_per_side)
+    points_x = np.tile(points_one_side[None, :], (n_per_side, 1))
+    points_y = np.tile(points_one_side[:, None], (1, n_per_side))
+    points = np.stack([points_x, points_y], axis=-1).reshape(-1, 2)
+    return points
+
+
+def adjust_grid(image, n_per_side=32):
+    # Generate regular grid
+    grid = build_point_grid(n_per_side)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    new_grid = []
+    height, width = image.shape
+    cell_width = width / n_per_side
+    cell_height = height / n_per_side
+
+    for i, point in enumerate(grid):
+        patch = image[int(point[1] * height):int(point[1] * height + cell_height),
+                int(point[0] * width):int(point[0] * width + cell_width)]
+
+        # Find contours in the patch
+        contours, _ = cv2.findContours(patch, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+            # Find the contour with the largest area
+            biggest_contour = max(contours, key=cv2.contourArea)
+
+            # Find the bounding box of the largest contour
+            x, y, w, h = cv2.boundingRect(biggest_contour)
+
+            # Crop the grayscale patch to the bounding box of the largest contour
+            symbol_patch = patch[y:y + h, x:x + w]
+
+            # Find the black point inside the cropped patch using cv2.minMaxLoc()
+            min_val, _, min_loc, _ = cv2.minMaxLoc(symbol_patch)
+            black_point = (int(min_loc[0] + x + point[0] * width),
+                           int(min_loc[1] + y + point[1] * height))
+
+            # Add the new point to the grid
+            new_grid.append(black_point)
+        else:
+            # Add the original point to the grid
+            new_grid.append([point[0] * width, point[1] * height])
+
+    # normalize grid
+    new_grid = np.array(new_grid).astype('float64')
+    new_grid[:, 0] = new_grid[:, 0] / width
+    new_grid[:, 1] = new_grid[:, 1] / height
+
+    return [new_grid]
+
 
 class MMsegHandler(BaseHandler):
 
@@ -18,27 +167,46 @@ class MMsegHandler(BaseHandler):
                                    str(properties.get('gpu_id')) if torch.cuda.
                                    is_available() else self.map_location)
         self.manifest = context.manifest
-        self.model = SamAutomaticMaskGenerator(arch='huge')
+        self.model = SamAutomaticMaskGenerator(arch='huge',
+                                               # crop_n_layers=2,
+                                               # crop_n_points_downscale_factor=1,
+                                               )
         self.initialized = True
-        logging.info('Init complete ...')
 
     def preprocess(self, data):
         images = []
 
         for row in data:
-            image = row.get('data') or row.get('body')
-            if isinstance(image, str):
-                image = base64.b64decode(image)
-            image = mmcv.imfrombytes(image)
+            request = row.get('data') or row.get('body')
+            url = request['url']
+            ocr_boxes = request['ocr_boxes']
+            if not url.startswith("https://"):
+                raise ValueError("Invalid S3 URL provided")
+
+            response = requests.get(url)
+            response.raise_for_status()
+
+            image = mmcv.imfrombytes(response.content)
+
+            if ocr_boxes:
+                image = remove_text(image, ocr_boxes)
+            # crop bottom and right side of the image on 12.5%
+            image = image[:int(image.shape[0] * 0.875), :int(image.shape[1] * 0.875)]
             images.append(image)
 
         return images
 
     def inference(self, data, *args, **kwargs):
-        logging.info('Start inference')
-        results = [self.model.generate(img) for img in data]
-        logging.info('Prediction finished')
-        logging.info(results)
+        step = 1024
+        window_size = 1024
+        # area_size_filter = [200, step*window_size/10]
+        area_size_filter = [200, 100 * 50]  # limit the size to double doors size
+        results = [sliding_window_inference(img, self.model, step=step, window_size=window_size,
+                                            area_size_filter=area_size_filter) for img in data]
+
+        # filter out white boxes
+        results = [skip_white_boxes(boxes, image) for boxes, image in zip(results, data)]
+
         return results
 
     def postprocess(self, data):
@@ -46,9 +214,7 @@ class MMsegHandler(BaseHandler):
 
         for image_result in data:
             output.append({
-                'bbox': [i['bbox'] for i in image_result],
-                'area': [i['area'] for i in image_result],
-                'stability_score': [i['stability_score'] for i in image_result],
+                'bbox': image_result,
+                # 'stability_score': image_result[1],
             })
-            logging.info(str(output))
         return output
